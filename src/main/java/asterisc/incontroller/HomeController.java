0f5534de7a3b2431cee58a1406f8d211eb21bad2
preeti tambe <preetitambe158@gmail.comgit config --global user.name priti>
2024-05-06 16:13:07 +0530

    Created Controller class

---
+++ src/main/java/asterisc/incontroller/HomeController.java
@@ -0,0 +1,15 @@
+package asterisc.incontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/test")
+	String myfun()
+	{
+		return "index.jsp";
+	}
+
+}
